a0d24bdc2cb55ac3f2a332b66caaaf06c20d4533
EthanBDonoho <58092997+EthanBDonoho@users.noreply.github.com>
2022-02-18 16:27:06 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,20 @@
+"""
+Ethan Donoho - February 2022
+API for use with a discord bot
+Tracks and evaluates trades made for profit
+"""
+
+def add_item(item, price):
+    print("added " + item + " for " + str(price))
+
+def recall_price(item):
+    print("price for " + item + " is ")
+
+def buy_item(item, price, amount, player):
+    print(player + " bought " + str(amount) + " " + item + " for " + str(price))
+
+def 
+
+add_item("Toys", 12)
+recall_price("Toys")
+buy_item("Toys", 12, 1, "RobinMasters")
